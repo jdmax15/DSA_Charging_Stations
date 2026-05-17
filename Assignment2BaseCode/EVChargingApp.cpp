409620ee09e98047053eebb695864d1c39e1ceb6
jdmax15 <joel.maxwell15@gmail.com>
2026-05-17 12:22:00 +1000

    Added a menu to Main()

--- Assignment2BaseCode/EVChargingApp.cpp
+++ Assignment2BaseCode/EVChargingApp.cpp
@@ -14,10 +14,44 @@ using namespace std;
 
 int main() {
 	EVCharging charging;
-	charging.printLocations();
-	charging.printAdjacencyMatrix();
-	charging.sortLocations();
-	charging.printAdjacentStations();
-	charging.findNearestStation();
+	int choice;
+
+	do {
+		cout << "\n--- Sydney EV Charging Menu ---" << endl;
+		cout << "1. Print locations" << endl;
+		cout << "2. Print adjacency matrix" << endl;
+		cout << "3. List stations by price" << endl;
+		cout << "4. Find adjacent charging stations" << endl;
+		cout << "5. Find nearest charging station" << endl;
+		cout << "0. Exit" << endl;
+		cout << "Enter choice: ";
+		cin >> choice;
+		cout << endl;
+
+		switch (choice) {
+		case 1:
+			charging.printLocations();
+			break;
+		case 2:
+			charging.printAdjacencyMatrix();
+			break;
+		case 3:
+			charging.sortLocations();
+			break;
+		case 4:
+			charging.printAdjacentStations();
+			break;
+		case 5:
+			charging.findNearestStation();
+			break;
+		case 0:
+			cout << "Exiting." << endl;
+			break;
+		default:
+			cout << "Invalid choice." << endl;
+			break;
+		}
+	} while (choice != 0);
+
 	return 0;
 }
